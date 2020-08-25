22df8e873a876de90ee284ba7b910f6cd3d9dc2c
Ana Paula Souza <anasouza@usp.br>
2020-08-25 17:32:20 -0300

    addind multiple questions in object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -90,7 +90,7 @@ export class AppComponent {
 
 
 import { Component, Input, Inject } from '@angular/core';
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
 import {AppService} from './app.service';
 
@@ -127,37 +127,49 @@ export class AppComponent {
   model = {};
   questions: any;
   form: any;
-  options: any;
-
-  private appService: AppService;
-  
+  options: any;  
 
   
-  public constructor(appService: AppService) {
-    this.appService = appService;
-    this.questions = this.appService.getQuestions()
+  public constructor(appService: AppService, formBuilder: FormBuilder) {
+    appService = appService;
+    this.questions = appService.getQuestions()
     /*.subscribe(
       questions => this.questions = questions,
       error => alert('Erro')
     );
     console.log(this.questions);*/
+
+    /*let nome: string = "variavel"
+    this.form = new FormGroup({
+      key: new FormControl("teste"),
+      [nome]: new FormControl("tem que ta variavel") 
+
+    })
+
+    console.log(this.form)*/
+
     this.reactiveForm = new FormArray(this.questions.map( (question) => {
       
-      for (let i = 0; i<question.options.length; i++) {
-        if (question.options[i].checked === true)
-          return  new FormControl(question.options[i].name)
+      if (question.type === 1){
+        for (let i = 0; i<question.options.length; i++) {
+          if (question.options[i].checked)
+            return  new FormControl(question.options[i].name)
+        }
       }
+
+      if (question.type === 2) {
+        var arrayForm: Array<FormArray> = [];
+
+        for (let i = 0; i<question.options.length; i++) {
+            arrayForm.push(question.options[i].checked);
+        }
+          return new FormControl(arrayForm)
+      }
+
       return null;
     }));
-  }
+  } 
 
-  /*public ngOnInit(){
-    this.questions = this.appService.getQuestions()
-    /*.subscribe(
-      questions => this.questions = questions,
-      error => alert('Erro')
-    )
-  }*/
 
 
   prevStep(step) {
